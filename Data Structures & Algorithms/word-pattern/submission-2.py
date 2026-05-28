10a765241f6232d2b1c70cb3056463ff33bf6b10
Hai Long Do Pham <95564336+hai-long05@users.noreply.github.com>
2026-05-28 19:59:22 +0200

    Add: word-pattern - submission-2

---
+++ Data Structures & Algorithms/word-pattern/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        p_map = {}
+        s_map = {}
+        s = s.split()
+
+        if len(pattern) != len(s):
+            return False
+        
+        for i in range(min(len(pattern), len(s))):
+            if (s[i] in s_map and s_map[s[i]] != pattern[i]) or (pattern[i] in p_map and p_map[pattern[i]] != s[i]):
+                return False
+            p_map[pattern[i]] = s[i]
+            s_map[s[i]] = pattern[i]
+
+        return True
