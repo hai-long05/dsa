b26f6839f793b0134bd88771dec723f2e30e502b
Hai Long Do Pham <95564336+hai-long05@users.noreply.github.com>
2026-06-18 22:22:30 +0200

    Add: valid-perfect-square - submission-2

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        for i in range(num + 1):
+            sq = i * i
+            if sq == num:
+                return True
+            if sq > num: 
+                return False
